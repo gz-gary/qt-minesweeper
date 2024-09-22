d0264ab0705a0d992437f3bb6e45ae70e7fde1ac
gz <jinlizhong@smail.nju.edu.cn>
2024-09-22 16:46:44 +0800

    implement mine generator

--- PlayDialog.cpp
+++ PlayDialog.cpp
@@ -1,8 +1,16 @@
 #include "PlayDialog.hpp"
+#include <QEvent>
+#include <iostream>
+
+bool PlayDialog::outOfBound(int x, int y) const
+{
+    return x < 0 || y < 0 || x >= setting.map_width || y >= setting.map_height;
+}
 
 void PlayDialog::applySetting(const DifficultySetting &setting)
 {
     this->setFixedSize(setting.map_width * 40, 150 + setting.map_height * 40);
+    this->setting = setting;
     grid = Grid(setting.map_width * 40, setting.map_height * 40, setting.map_width, setting.map_height);
     for (auto _ : grid_btn_mine) {
         for (auto btn : _) {
@@ -12,13 +20,78 @@ void PlayDialog::applySetting(const DifficultySetting &setting)
     grid_btn_mine.clear();
     grid_btn_mine.resize(setting.map_width);
     for (auto &column : grid_btn_mine) column.resize(setting.map_height);
+
+    for (auto _ : grid_lbl) {
+        for (auto lbl : _) {
+            delete lbl;
+        }
+    }
+    grid_lbl.clear();
+    grid_lbl.resize(setting.map_width);
+    for (auto &column : grid_lbl) column.resize(setting.map_height);
+
+    grid_info.resize(setting.map_width);
+    for (auto &column : grid_info) column.resize(setting.map_height);
+
+    is_first_click = true;
+
     for (int i = 0; i < setting.map_width; ++i) {
         for (int j = 0; j < setting.map_height; ++j) {
             QPushButton *btn = new QPushButton(this);
+            btn->installEventFilter(this);
             btn->setGeometry(i * 40, 150 + j * 40, 40, 40);
             btn->show();
             grid_btn_mine[i][j] = btn;
+
+            QLabel *lbl = new QLabel(this);
+
+            grid_info[i][j].is_clear = false;
+            grid_info[i][j].is_mine = false;
+            grid_info[i][j].cnt_mines_around = 0;
         }
+    }
+}
+
+void PlayDialog::generateMines(int cl_x, int cl_y)
+{
+    srand(time(0));
+
+    for (int i = 0; i < setting.cnt_mine; ++i) {
+        int p = rand() % (setting.map_width * setting.map_height - i);
+        for (int j = 0; j < setting.map_width; ++j) {
+            for (int k = 0; k < setting.map_height; ++k) {
+                if (grid_info[j][k].is_mine) continue;
+                if (j == cl_x && k == cl_y) continue;
+                if (p == 0) {
+                    grid_info[j][k].is_mine = true;
+                }
+                --p;
+            }
+        }
+    }
+
+    for (int i = 0; i < setting.map_width; ++i) {
+        for (int j = 0; j < setting.map_height; ++j) {
+            if (!grid_info[i][j].is_mine) {
+                for (int k = 0; k < 8; ++k) {
+                    int di = i + DirectionOffset[k][0], dj = j + DirectionOffset[k][1];
+                    if (!outOfBound(di, dj) && grid_info[di][dj].is_mine) {
+                        ++grid_info[i][j].cnt_mines_around;
+                    }
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < setting.map_width; ++i) {
+        for (int j = 0; j < setting.map_height; ++j) {
+            if (grid_info[i][j].is_mine) {
+                putchar('*');
+            } else {
+                putchar('0' + grid_info[i][j].cnt_mines_around);
+            }
+        }
+        putchar('\n');
     }
 }
 
@@ -39,5 +112,25 @@ PlayDialog::~PlayDialog()
 
 bool PlayDialog::eventFilter(QObject *target, QEvent *event)
 {
+    if (event->type() == QEvent::MouseButtonRelease) {
+        bool click_btn = false;
+        int cl_x, cl_y;
+        for (int i = 0; i < setting.map_width; ++i) {
+            for (int j = 0; j < setting.map_height; ++j) {
+                if (grid_btn_mine[i][j] == target) {
+                    click_btn = true;
+                    cl_x = i, cl_y = j;
+                    break;
+                }
+            }
+            if (click_btn) break;
+        }
+        if (click_btn) {
+            if (is_first_click) {
+                generateMines(cl_x, cl_y);
+                is_first_click = false;
+            }
+        }
+    }
     return QDialog::eventFilter(target, event);
 }
--- include/PlayDialog.hpp
+++ include/PlayDialog.hpp
@@ -6,13 +6,35 @@
 #include <QLabel>
 #include <vector>
 
+const int DirectionOffset[8][2] = {
+    {1, 0},
+    {0, 1},
+    {-1, 0},
+    {0, -1},
+    {1, 1},
+    {1, -1},
+    {-1, 1},
+    {-1, -1},
+};
+
 class PlayDialog : public QDialog {
 private:
+    struct GridInfo {
+        bool is_mine;
+        bool is_clear;
+        int cnt_mines_around;
+    };
+    bool outOfBound(int x, int y) const;
+    void generateMines(int cl_x, int cl_y);
 
 public:
 
     Grid grid;
+    DifficultySetting setting;
     std::vector<std::vector<QPushButton*>> grid_btn_mine;
+    std::vector<std::vector<QLabel*>> grid_lbl;
+    std::vector<std::vector<GridInfo>> grid_info;
+    bool is_first_click;
 
     void applySetting(const DifficultySetting& setting);
 
